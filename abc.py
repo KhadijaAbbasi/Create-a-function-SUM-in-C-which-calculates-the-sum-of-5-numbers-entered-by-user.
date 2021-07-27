1d07f3478d8eb1f76ccbc1496108e5403f9a80b2
KhadijaAbbasi <85893701+KhadijaAbbasi@users.noreply.github.com>
2021-07-27 16:21:07 +0500

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,12 @@
+def cal():
+    sum=0
+    for i in range(5):
+         no=int(input("Enter number: "))
+         sum=sum+no
+    print("SUM= ",sum)
+
+def main():
+    cal()
+if __name__== "__main__":
+    main()
+
